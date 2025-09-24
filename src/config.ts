32e96b2ae9e1694b318a31efdf0e73364e18447e
TeTo <104202954+TeTo-O@users.noreply.github.com>
2025-09-24 23:00:21 +0200

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,56 @@
+import type { NavItems } from "./types";
+
+export const NAV_ITEMS: NavItems = {
+	home: {
+		path: "/",
+		title: "home",
+	},
+	blog: {
+		path: "/blog",
+		title: "blog",
+	},
+	tags: {
+		path: "/tags",
+		title: "tags",
+	},
+	/*media: {
+		path: "/media",
+		title: "media",
+	},*/
+	about: {
+		path: "/about",
+		title: "about",
+	},
+};
+
+export const SITE = {
+	// Your site's detail?
+	name: "TeTo",
+	title: "TeTo's blog",
+	url: "",
+	githubUrl: "https://github.com/TeTo-O",
+	listDrafts: true,
+	image: "/assets/TETOlogo.svg",
+	// YT video channel Id (used in media.astro)
+	ytChannelId: "",
+	// Optional, user/author settings (example)
+	// Author: name
+	author: "TeTo", // Example: Fred K. Schott
+	// Author: Twitter handler
+	authorGithub: "https://github.com/TeTo-O", // Example: FredKSchott
+	// Author: Image external source
+	authorImage: "/assets/TETOlogo.svg", // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
+	// Author: Bio
+	authorBio:
+		"Web Security Student - CTF Player",
+};
+
+// Ink - Theme configuration
+export const PAGE_SIZE = 8;
+export const USE_POST_IMG_OVERLAY = false;
+export const USE_MEDIA_THUMBNAIL = false;
+
+export const USE_AUTHOR_CARD = true;
+export const USE_SUBSCRIPTION = false; /* works only when USE_AUTHOR_CARD is true */
+
+export const USE_VIEW_STATS = false;
---
+++ src/pages/_astro-ink.md
@@ -0,0 +1,4 @@
+# Welcome, I’m **TeTo**! 👋 
+BSc graduate in Cybersecurity - Web Security Student - CTF player.  
+This is my little corner of the web where I share **write-ups**, **projects**, and anything interesting I stumble upon in my journey. 
+                
